2c411fd88a504f840376a1d46118ca03f11fad03
David Bonting <david.bonting@hotmail.com>
2019-07-24 13:10:53 +0200

    Improvements

--- files/index.js
+++ files/index.js
@@ -1,5 +1,22 @@
+var mouseOver = false;
+var seconds = 0;
+var playing = false;
+var timerInterval = null;
+
+var pointers = [
+    [0, 10],
+    [3, 60],
+    [4, 60],
+    [5, 70],
+    [6, 70],
+    [9, 20],
+    [10, 20],
+];
+
 // hande mouse over
 document.getElementsByClassName('wrapper')[0].addEventListener('mousemove', function(e) {
+    mouseOver = true;
+
     elementWidth = this.offsetWidth;
     pxHover = event.pageX- this.offsetLeft;
     percentageHover = (pxHover / elementWidth) * 100;
@@ -18,6 +35,21 @@ document.getElementsByClassName('wrapper')[0].addEventListener('click', function
     for (var i = 0; i < playButtons.length; i++) {
         playButtons[i].click();
     }
+
+    if(!playing){
+        playing = true;
+        seconds = 0;
+        timerInterval = setInterval(increaseSeconds, 1000);
+    }
+
+    // should be used in production
+    // if(playing){
+    //     playing = false;
+    //     clearInterval(timerInterval);
+    // }else{
+    //     playing = true;
+    //     timerInterval = setInterval(increaseSeconds, 1000);
+    // }
 });
 
 // reset on leave
@@ -26,9 +58,27 @@ document.getElementsByClassName('wrapper')[0].addEventListener("mouseout", setCl
 
 function setClipPath(percentageHover){
     if(percentageHover.target){
+        mouseOver = false;
         percentageHover = 50;
     }
 
     rightElement = document.getElementsByClassName('right')[0];
     rightElement.style.clipPath = 'polygon(0 0, ' + percentageHover + '% 0, ' + percentageHover + '% 100%, 0% 100%)';
+}
+
+function increaseSeconds() {
+    handleAnimatedPoints();
+
+    seconds = seconds + 1;
+}
+
+function handleAnimatedPoints(){
+    pointers.forEach(function(pointer, key){
+        if(pointer[0]==seconds && !mouseOver && pointers[key + 1]!=undefined){
+            timeToAnimate = pointers[key + 1][0] - pointer[0];
+            rightElement = document.getElementsByClassName('right')[0];
+            rightElement.style.transitionDuration = timeToAnimate + 's';
+            setClipPath(pointer[1]);
+        }
+    });
 }
