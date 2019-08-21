6974aa0545ddb0c6e4bda02437d67b7507b0c640
David Bonting <david.bonting@hotmail.com>
2019-08-21 15:04:33 +0200

    Remove duplicate code

--- files/index.js
+++ files/index.js
@@ -1,12 +1,11 @@
 (function(){
     var pointers = [
         [0, 50],
-        [0.001, 10],
-        [0.25, 20],
-        [0.5, 30],
-        [0.7, 40],
-        [1.25, 50],
-        [3, 60],
+        [0.5, 90],
+        [1, 10],
+        [2, 30],
+        [2.5, 60],
+        [3, 50],
         [4, 60],
         [5, 70],
         [6, 70],
@@ -20,7 +19,6 @@
     var mouseOver = false;
     var lastPointMin = 0;
     var lastPointMax = 0;
-    var device = detectDevice();
     var outOfSyncCorrection = false;
     var dragging = false;
     var videoPlayerSize = 0;
@@ -100,6 +98,33 @@
         }
     }
 
+    function handleDrag(e){
+        mouseOver = true;
+
+        // disable animation
+        rightElement = document.getElementById('vice-split-player-nn').getElementsByClassName('rightFrame')[0];
+        rightElement.style.transitionDuration = '0s';
+    
+        // calculate clip position
+        elementWidth = this.offsetWidth;
+
+        if(e.type==="mousemove"){
+            pxHover = event.pageX - this.offsetLeft;
+        }else{
+            pxHover = e.touches[0].pageX - this.offsetLeft;
+        }
+
+        percentageHover = (pxHover / elementWidth) * 100;
+    
+        // fix ugly last bit
+        if (percentageHover >= 99.5) {
+            percentageHover = 100;
+        }
+    
+        setClipPath(percentageHover);
+    }
+
+    // handle clicks
     document.getElementById('vice-split-player-nn').addEventListener('click', handleClick);
     document.getElementById('vice-split-player-nn').addEventListener('touchend', handleClick);
 
@@ -112,49 +137,8 @@
     });
 
     // hande mouse over
-    document.getElementById('vice-split-player-nn').addEventListener('mousemove', function(e) {
-        if(device==='desktop'){
-            mouseOver = true;
-
-            // disable animation
-            rightElement = document.getElementById('vice-split-player-nn').getElementsByClassName('rightFrame')[0];
-            rightElement.style.transitionDuration = '0s';
-        
-            // calculate clip position
-            elementWidth = this.offsetWidth;
-            pxHover = event.pageX - this.offsetLeft;
-            percentageHover = (pxHover / elementWidth) * 100;
-        
-            // fix ugly last bit
-            if (percentageHover >= 99.5) {
-                percentageHover = 100;
-            }
-        
-            setClipPath(percentageHover);
-        }
-    });
-
-    document.getElementById('vice-split-player-nn').addEventListener('touchmove', function(e) {
-        if(device!='desktop'){
-            mouseOver = true;
-
-            // disable animation
-            rightElement = document.getElementById('vice-split-player-nn').getElementsByClassName('rightFrame')[0];
-            rightElement.style.transitionDuration = '0s';
-        
-            // calculate clip position
-            elementWidth = this.offsetWidth;
-            pxHover = e.touches[0].pageX - this.offsetLeft;
-            percentageHover = (pxHover / elementWidth) * 100;
-        
-            // fix ugly last bit
-            if (percentageHover >= 99.5) {
-                percentageHover = 100;
-            }
-        
-            setClipPath(percentageHover);
-        }
-    });
+    document.getElementById('vice-split-player-nn').addEventListener('mousemove', handleDrag);
+    document.getElementById('vice-split-player-nn').addEventListener('touchmove', handleDrag);
 
     // reset on leave
     document.getElementById('vice-split-player-nn').addEventListener("mouseleave", setClipPath);
@@ -166,7 +150,6 @@
             mouseOver = false;
             handleAnimatedPoints(lastPointMin);
         }
-
         
         rightElement = document.getElementById('vice-split-player-nn').getElementsByClassName('rightFrame')[0];
         rightElement.style.width = videoPlayerSize * (percentageHover / 100) + 'px';
@@ -190,21 +173,5 @@
                 }
             }
         }
-    }
-
-    function detectDevice(){
-        var mobile = false;
-        var tablet = false;
-        var desktop = false;
-        
-        var a = window.navigator.userAgent||window.navigator.vendor||window.opera;
-        
-        if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) mobile = true;
-        if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) tablet = true;
-        if(mobile === false && tablet === false) desktop = true;
-        
-        if(mobile === true) { return 'mobile'; }
-        if(mobile === false && tablet === true) { return 'tablet'; }
-        if(mobile === false && tablet === false) { return 'desktop'; }
     }
 })();
